7848b5a7330fa6167c13e23f6b7f5001f2627f70
tony225225 <110821007+tony225225@users.noreply.github.com>
2022-08-11 13:05:41 +0530

    Add files via upload

---
+++ sgn.js
@@ -0,0 +1,9 @@
+function formvalidation(){
+    var email = document.validate.email.value;
+  //  var pass = document.validate.pass.value;
+    if (email==null || email== ""){
+        alert("enter email")
+    }
+    
+}
+
